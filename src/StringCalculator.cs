b1520a69ffe71bc4e226bb835f26f3b15b734013
Jeffrey Cumpsty <jeff@cumpsty.co.uk>
2024-05-17 08:10:37 +0200

    tests satisfied

--- src/StringCalculator.cs
+++ src/StringCalculator.cs
@@ -7,11 +7,34 @@ public class StringCalculator
     if (string.IsNullOrEmpty(numbers))
       return 0;
 
-    var numberArray = numbers.Split(',');
+    var delimiter = ",";
+    if (numbers.StartsWith("//"))
+    {
+      var delimiterEndIndex = numbers.IndexOf("\n");
+      delimiter = numbers.Substring(2, delimiterEndIndex - 2);
+      numbers = numbers.Substring(delimiterEndIndex + 1);
+    }
 
-    if (numberArray.Length == 1)
-      return int.Parse(numberArray[0]);
+    var numberArray = numbers.Split(new[] { delimiter, ",", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+    var negatives = new List<int>(); // List to store negative numbers. 
 
-    return int.Parse(numberArray[0]) + int.Parse(numberArray[1]);
+    var sum = 0;
+    foreach (var numStr in numberArray)
+    {
+        var num = int.Parse(numStr);
+        if (num < 0)
+            negatives.Add(num);
+        else if (num <= 1000) // Check if number is less than or equal to 1000
+            sum += num; 
+    }
+
+    if (negatives.Any()) // If any negative numbers were found.
+    {
+        var negativeNumbersString = string.Join(",", negatives);
+        throw new ArgumentException("negatives not allowed: " + negativeNumbersString);
+    }
+
+    return sum;
   }
+
 }
--- tests/StringCalculatorTests.cs
+++ tests/StringCalculatorTests.cs
@@ -33,5 +33,53 @@ public class StringCalculatorTests
     var result = _calculator.Add("3,8");
     Assert.AreEqual(11, result);
   }
-}
 
+  [Test]
+  public void Add_GivenFourNumbers_ReturnsSum()
+  {
+    var result = _calculator.Add("1,2,3,4");
+    Assert.AreEqual(10, result);
+  }
+
+  [Test]
+  public void Add_GivenTenNumbers_ReturnsSum()
+  {
+    var result = _calculator.Add("1,2,3,4,5,6,7,8,9,10");
+    Assert.AreEqual(55, result);
+  }
+
+  [Test]
+  public void Add_GivenNumbersWithNewlines_ReturnsSum()
+  {
+    var result = _calculator.Add("1\n2,3");
+    Assert.AreEqual(6, result);
+  }
+
+  [Test]
+  public void Add_GivenNumbersWithCustomDelimiter_ReturnsSum()
+  {
+    var result = _calculator.Add("//;\n1;2");
+    Assert.AreEqual(3, result);
+  }
+
+  [Test]
+  public void Add_GivenNumbersWithCustomDelimiterAndNewlines_ReturnsSum()
+  {
+    var result = _calculator.Add("//$\n1$2\n3");
+    Assert.AreEqual(6, result);
+  }
+
+  [Test]
+  public void Add_GivenNegativeNumbers_ThrowsExceptionWithMessage()
+  {
+    var ex = Assert.Throws<ArgumentException>(() => _calculator.Add("-1,2,-3"));
+    Assert.AreEqual("negatives not allowed: -1,-3", ex.Message);
+  }
+
+  [Test]
+  public void Add_GivenNumberGreaterThan1000_IgnoresNumber()
+  {
+    var result = _calculator.Add("2,1001");
+    Assert.AreEqual(2, result);
+  }
+}
